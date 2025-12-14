a8c3a2186eeff6a7318778ef3dccf12fea44a252
Sadvik <sadvikgowda3333@gmail.com>
2025-12-14 14:27:22 +0530

    update

--- src/com/it/problem_solve/leetcode/LongestPalindrome.java
+++ src/com/it/problem_solve/leetcode/LongestPalindrome.java
@@ -9,10 +9,11 @@ public class LongestPalindrome {
     public static void main(String[] args) {
         String s = "babad";
         String s2 = "cbb";
-        System.out.println(solve(s));
-        System.out.println(solve(s2));
+        System.out.println(solve2(s));
+        System.out.println(solve2(s2));
     }
 
+    //Brute fore Time complexity : O(n^3). there are n2 sub string
     public static String solve(String s) {
         String longestPal = "";
         int len = s.length();
@@ -41,4 +42,37 @@ public class LongestPalindrome {
         }
         return true;
     }
+
+
+    //Time complexity : O(n^2).  Since expanding a palindrome around its center could take O(n) time, the overall complexity is O(n^2).
+    public static String solve2(String s) {
+        int len = s.length();
+
+        String res = "";
+        for (int i = 0; i < len; i++) {
+
+            String oddPal = expandFromCenter(s, i, i); //odd palindrome
+            String evenPal = expandFromCenter(s, i, i + 1); //even palindrome
+
+            if (oddPal.length() > res.length()) {
+                res = oddPal;
+            }
+            if (evenPal.length() > res.length()) {
+                res = evenPal;
+            }
+
+        }
+        return res;
+    }
+
+
+    private static String expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+
+
 }
